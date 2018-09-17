eb2533eb1d7909ad2a3c47ce84fca52873a349f6
Devulak <Devulak@users.noreply.github.com>
2018-09-17 10:08:46 +0200

    Generalized some code after solution was found

--- EkstraOpgave01/EkstraOpgave01/EkstraOpgave01/Labyrinth.cs
+++ EkstraOpgave01/EkstraOpgave01/EkstraOpgave01/Labyrinth.cs
@@ -9,6 +9,107 @@ namespace EkstraOpgave01
     class Labyrinth
     {
         private bool[,] Grid;
-        private List<int[]> path;
+        private List<int[]> Path;
+        internal int[] Start;
+        internal int[] Finish;
+
+        public Labyrinth(int size)
+        {
+            Grid = new bool[size, size];
+        }
+
+        public void GeneratePath()
+        {
+            Random rnd = new Random();
+            List<int[]> pathTaken = new List<int[]>();
+            pathTaken.Add(Start);
+
+            while (!pathTaken.Any(p => p.SequenceEqual(Finish)))
+            {
+                // Check all 4 blocks around
+                List<int[]> availablePath = new List<int[]>();
+
+                for (int y = -1; y <= 1; y++)
+                {
+                    for (int x = -1; x <= 1; x++)
+                    {
+                        if (x == 0 && y == 0) // Center
+                        {
+                            continue;
+                        }
+                        else if (Math.Abs(x) == Math.Abs(y)) // Corner
+                        {
+                            continue;
+                        }
+                        else // Side
+                        {
+                            try
+                            {
+                                int xCoords = pathTaken.Last()[0] + x;
+                                int yCoords = pathTaken.Last()[1] + y;
+                                //bool aa = Labyrinth[xCoords, yCoords];
+                                if (xCoords >= 0 && xCoords < Grid.GetLength(0) && yCoords >= 0 && yCoords < Grid.GetLength(1))
+                                {
+                                    availablePath.Add(new int[] { xCoords, yCoords });
+                                }
+                            }
+                            catch (IndexOutOfRangeException)
+                            {
+                                // out of bounce
+                            }
+                        }
+                    }
+                }
+
+                // Next step
+                int[] nextStep = availablePath[rnd.Next(0, availablePath.Count)];
+
+                if (pathTaken.Any(p => p.SequenceEqual(nextStep))) // is the next step a part of the current path?
+                {
+                    int index = pathTaken.FindIndex(p => p[0] == nextStep[0] && p[1] == nextStep[1]);
+                    pathTaken = pathTaken.GetRange(0, index + 1);
+                }
+                else if (false) // Is the next step next to two current paths? (eleminating the already created path)
+                {
+
+                }
+                else
+                {
+                    pathTaken.Add(nextStep);
+                }
+            }
+            
+            foreach (int[] step in pathTaken)
+            {
+                Grid[step[0], step[1]] = true;
+            }
+            Path = pathTaken;
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < Grid.GetLength(1); i++)
+            {
+                for (int j = 0; j < Grid.GetLength(0); j++)
+                {
+                    if (Start != null && j == Start[0] && i == Start[1])
+                    {
+                        Console.BackgroundColor = ConsoleColor.Blue;
+                    }
+                    else if (Finish != null && j == Finish[0] && i == Finish[1])
+                    {
+                        Console.BackgroundColor = ConsoleColor.Red;
+                    }
+                    else if (Grid[j, i])
+                    {
+                        Console.BackgroundColor = ConsoleColor.Green;
+                    }
+                    Console.Write(Grid[j, i] ? "X" : "O");
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
     }
 }
--- EkstraOpgave01/EkstraOpgave01/EkstraOpgave01/Program.cs
+++ EkstraOpgave01/EkstraOpgave01/EkstraOpgave01/Program.cs
@@ -8,94 +8,21 @@ namespace EkstraOpgave01
 {
     class Program
     {
-        private static bool[,] Labyrinth;
         static void Main(string[] args)
         {
             int size = RequestInt("What size would you like the labyrinth to be ?");
-            Labyrinth = new bool[size, size];
-            PrintLabyrinth(Labyrinth);
+            Labyrinth lab = new Labyrinth(size);
 
-            int[] start = RequestCoordinates(Labyrinth, "Where would you like to start?");
-            PrintLabyrinth(Labyrinth, start);
-            int[] finish = RequestCoordinates(Labyrinth, "Where would you like to finish?");
-            PrintLabyrinth(Labyrinth, start, finish);
+            lab.Start = RequestCoordinates(size, "Where would you like to start?");
+            lab.Finish = RequestCoordinates(size, "Where would you like to finish?");
 
-            GeneratePath(start, finish);
+            lab.GeneratePath();
+            lab.Print();
 
             Console.ReadKey();
         }
 
-        private static void GeneratePath(int[] start, int[] finish)
-        {
-            Random rnd = new Random();
-            List<int[]> pathTaken = new List<int[]>();
-            pathTaken.Add(start);
-
-            while (!pathTaken.Any(p => p.SequenceEqual(finish)))
-            {
-                // Check all 4 blocks around
-                List<int[]> availablePath = new List<int[]>();
-
-                for (int y = -1; y <= 1; y++)
-                {
-                    for (int x = -1; x <= 1; x++)
-                    {
-                        if (x == 0 && y == 0) // Center
-                        {
-                            continue;
-                        }
-                        else if (Math.Abs(x) == Math.Abs(y)) // Corner
-                        {
-                            continue;
-                        }
-                        else // Side
-                        {
-                            try
-                            {
-                                int xCoords = pathTaken.Last()[0] + x;
-                                int yCoords = pathTaken.Last()[1] + y;
-                                //bool aa = Labyrinth[xCoords, yCoords];
-                                if (xCoords >= 0 && xCoords < Labyrinth.GetLength(0) && yCoords >= 0 && yCoords < Labyrinth.GetLength(1))
-                                {
-                                    availablePath.Add(new int[] { xCoords, yCoords });
-                                }
-                            }
-                            catch (IndexOutOfRangeException)
-                            {
-                                // out of bounce
-                            }
-                        }
-                    }
-                }
-
-                // Next step
-                int[] nextStep = availablePath[rnd.Next(0, availablePath.Count)];
-
-                if(pathTaken.Any(p => p.SequenceEqual(nextStep)))
-                {
-                    int index = pathTaken.FindIndex(p => p[0] == nextStep[0] && p[1] == nextStep[1]);
-                    pathTaken = pathTaken.GetRange(0, index + 1);
-                }
-                else
-                {
-                    pathTaken.Add(nextStep);
-                }
-
-                //PrintLabyrinth(Labyrinth, pathTaken.Last(), finish);
-                //Console.WriteLine(pathTaken.Count);
-                //Console.ReadKey();
-            }
-
-            Console.WriteLine("===End result===");
-            foreach (int[] step in pathTaken)
-            {
-                Labyrinth[step[0], step[1]] = true;
-            }
-            PrintLabyrinth(Labyrinth, start, finish);
-            Console.WriteLine("Found a path!");
-        }
-
-        private static int[] RequestCoordinates(bool[,] labyrinth, string question)
+        private static int[] RequestCoordinates(int size, string question)
         {
             int[] output = new int[2];
             string[] inputSplit = new string[0];
@@ -116,14 +43,14 @@ namespace EkstraOpgave01
                     Console.WriteLine("Either or both are not a number!");
                     completed = false;
                 }
-                else if (output[0] != 0 && output[0] != labyrinth.GetLength(0) - 1 && output[1] != 0 && output[1] != labyrinth.GetLength(1) - 1) // is not an end point of the labyrinth?
+                else if (output[0] >= 0 && output[0] < size && output[1] >= 0 && output[1] < size) // is not an end point of the labyrinth?
                 {
-                    Console.WriteLine("Doesn't hit an end of the labyrinth!");
-                    completed = false;
+                    completed = true;
                 }
                 else
                 {
-                    completed = true;
+                    Console.WriteLine("Doesn't hit an end of the labyrinth!");
+                    completed = false;
                 }
             }
             return output;
@@ -137,32 +64,6 @@ namespace EkstraOpgave01
                 Console.WriteLine(question);
             } while (!int.TryParse(Console.ReadLine(), out output));
             return output;
-        }
-
-        private static void PrintLabyrinth(bool[,] labyrinth, int[] start = null, int[] finish = null)
-        {
-            for (int i = 0; i < labyrinth.GetLength(1); i++)
-            {
-                for (int j = 0; j < labyrinth.GetLength(0); j++)
-                {
-                    if (start != null && j == start[0] && i == start[1])
-                    {
-                        Console.BackgroundColor = ConsoleColor.Blue;
-                    }
-                    else if (finish != null && j == finish[0] && i == finish[1])
-                    {
-                        Console.BackgroundColor = ConsoleColor.Red;
-                    }
-                    else if(labyrinth[j, i])
-                    {
-                        Console.BackgroundColor = ConsoleColor.Green;
-                    }
-                    Console.Write(labyrinth[j, i] ? "X" : "O");
-                    Console.BackgroundColor = ConsoleColor.Black;
-                    Console.Write(" ");
-                }
-                Console.WriteLine();
-            }
         }
     }
 }
