4d01f85c25395f8b51556092cb9ac11229cc97cf
Devulak <Devulak@users.noreply.github.com>
2018-10-05 08:53:04 +0200

    Ex12

---
+++ Ex12/Ex12-Timer/Ex12-Timer/Timer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex12_Timer
+{
+    public class Timer
+    {
+        public int Seconds;
+
+        override
+        public string ToString()
+        {
+            int seconds = Seconds % 60;
+            int minutes = Seconds / 60 % 60;
+            int hours = Seconds / 60 / 60 % 60;
+            return string.Format("{2:00}:{1:00}:{0:00}", seconds, minutes, hours);
+        }
+
+        public string Padded(int v)
+        {
+            return string.Format("{0:00}", v);
+        }
+    }
+}
---
+++ Ex12/Ex12-Timer/TimerTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TimerTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TimerTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("42b4e339-b7fe-4e81-b550-c77b1523a851")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
