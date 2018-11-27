96af838a759f0a3ed85ef1254d0f99db742103eb
Devulak <Devulak@users.noreply.github.com>
2018-11-27 11:35:20 +0100

    Start Ex28

---
+++ Ex28/Ex28/Ex28/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Run();
+        }
+
+        private void Run()
+        {
+
+        }
+
+    }
+}
