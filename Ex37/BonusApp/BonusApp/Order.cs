0cdb76d07febc41a42b493c87bddb38156c62706
Devulak <Devulak@users.noreply.github.com>
2019-01-30 00:03:02 +0100

    Complete part of Ex37

---
+++ Ex37/BonusApp/BonusApp/Order.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BonusApp
+{
+    public class Order
+    {
+        private readonly List<Product> products = new List<Product>();
+
+        public BonusProvider Bonus { get; set; }
+
+        public void AddProduct(Product product)
+        {
+            products.Add(product);
+        }
+
+        public double GetValueOfProducts()
+        {
+            double total = 0;
+
+            products.ForEach(o => total += o.Value);
+
+            return total;
+        }
+
+        public double GetBonus()
+        {
+            return Bonus(GetValueOfProducts());
+        }
+
+        public double GetTotalPrice()
+        {
+            return GetValueOfProducts() - GetBonus();
+        }
+    }
+}
