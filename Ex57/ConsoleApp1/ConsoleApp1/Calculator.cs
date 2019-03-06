a2281d0af6812b2e7e4c545b7f47ff8d189955e7
Devulak <Devulak@users.noreply.github.com>
2019-03-06 11:52:35 +0100

    Too lazy to deal with the last

---
+++ Ex57/ConsoleApp1/ConsoleApp1/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Calculator
+    {
+        public static double Calculate(string expression)
+        {
+            expression = expression.Replace(" ", string.Empty); // Remove spaces
+
+            throw new NotImplementedException();
+        }
+    }
+}
