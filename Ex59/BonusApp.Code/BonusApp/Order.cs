e97322cfeb54736862897df9173c6ce193b1e92b
Devulak <Devulak@users.noreply.github.com>
2019-03-12 10:25:29 +0100

    Complete Ex59

---
+++ Ex59/BonusApp.Code/BonusApp/Order.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BonusApp
+{
+    public class Order
+    {
+        private List<Product> _products = new List<Product>();
+
+        public BonusProvider Bonus { get; set; }
+
+        public void AddProduct(Product p)
+        {
+            _products.Add(p);
+        }
+
+        public double GetValueOfProducts()
+        {
+            double valueOfProducts = 0.0;
+
+            foreach (Product p in _products)
+            {
+                valueOfProducts += p.Value;
+            }
+
+            return valueOfProducts;
+        }
+
+        public double GetBonus()
+        {
+            return Bonus(GetValueOfProducts());
+        }
+
+        //public double GetBonus(BonusProvider bonusProvider)
+        //{
+        //    return bonusProvider(GetValueOfProducts());
+        //}
+
+        public double GetBonus(Func<double, double> bonusProvider)
+        {
+            return bonusProvider(GetValueOfProducts());
+        }
+
+        public double GetTotalPrice()
+        {
+            return GetValueOfProducts() - GetBonus();
+        }
+
+        public double GetTotalPrice(Func<double, double> bonusProvider)
+        {
+            return GetValueOfProducts() - GetBonus(bonusProvider);
+        }
+    }
+}
---
+++ Ex59/BonusApp.Code/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using BonusApp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Order order;
+
+        [TestInitialize]
+        public void InitializeTest()
+        {
+            order = new Order();
+            order.AddProduct(new Product
+            {
+                Name = "Mælk",
+                Value = 10.0
+            });
+            order.AddProduct(new Product
+            {
+                Name = "Smør",
+                Value = 15.0
+            });
+            order.AddProduct(new Product
+            {
+                Name = "Pålæg",
+                Value = 20.0
+            });
+        }
+        [TestMethod]
+        public void TenPercent_Test()
+        {
+            Assert.AreEqual(4.5, Bonuses.TenPercent(45.0));
+            Assert.AreEqual(40.0, Bonuses.TenPercent(400.0));
+        }
+        [TestMethod]
+        public void FlatTwoIfAMountMoreThanFive_Test()
+        {
+            Assert.AreEqual(2.0, Bonuses.FlatTwoIfAmountMoreThanFive(10.0));
+            Assert.AreEqual(0.0, Bonuses.FlatTwoIfAmountMoreThanFive(4.0));
+        }
+        [TestMethod]
+        public void GetValueOfProducts_Test()
+        {
+            Assert.AreEqual(45.0, order.GetValueOfProducts());
+        }
+        [TestMethod]
+        public void GetBonus_Test()
+        {
+            order.Bonus = Bonuses.TenPercent;
+            Assert.AreEqual(4.5, order.GetBonus());
+
+            order.Bonus = Bonuses.FlatTwoIfAmountMoreThanFive;
+            Assert.AreEqual(2.0, order.GetBonus());
+        }
+        [TestMethod]
+        public void GetTotalPrice_Test()
+        {
+            order.Bonus = Bonuses.TenPercent;
+            Assert.AreEqual(40.5, order.GetTotalPrice());
+
+            order.Bonus = Bonuses.FlatTwoIfAmountMoreThanFive;
+            Assert.AreEqual(43.0, order.GetTotalPrice());
+        }
+        [TestMethod]
+        public void GetBonusAnonymous_Test()
+        {
+            order.Bonus = delegate (double amount) // <- Change to anonymous delegate
+            {
+                return amount / 10.0;
+            };
+            Assert.AreEqual(4.5, order.GetBonus());
+
+            order.Bonus = delegate (double amount) // <- Change to anonymous delegate
+            {
+                return amount > 5.0 ? 2.0 : 0.0;
+            };
+            Assert.AreEqual(2.0, order.GetBonus());
+        }
+
+        [TestMethod]
+        public void GetBonusLambda_Test()
+        {
+            order.Bonus = order.Bonus = (amount) => amount / 10.0; // <- Change to lambda expression
+            Assert.AreEqual(4.5, order.GetBonus());
+
+            order.Bonus = (amount) => amount > 5.0 ? 2.0 : 0.0; // <- Change to lambda expression
+            Assert.AreEqual(2.0, order.GetBonus());
+        }
+
+        [TestMethod]
+        public void GetBonusByLambdaParameter_Test()
+        {
+            // Use TenPercent lambda expresssion as parameter to GetBonus
+            Assert.AreEqual(4.5, order.GetBonus(Bonuses.TenPercent));
+
+            // Use FlatTwoIfAmountMoreThanFive lambda expresssion as parameter to GetBonus
+            Assert.AreEqual(2.0, order.GetBonus(Bonuses.FlatTwoIfAmountMoreThanFive));
+
+            // Use TenPercent lambda expresssion as parameter to GetBonus
+            Assert.AreEqual(4.5, order.GetBonus((amount) => amount / 10.0));
+
+            // Use FlatTwoIfAmountMoreThanFive lambda expresssion as parameter to GetBonus
+            Assert.AreEqual(2.0, order.GetBonus((amount) => amount > 5.0 ? 2.0 : 0.0));
+        }
+
+        [TestMethod]
+        public void GetTotalPriceByLambdaParameter_Test()
+        {
+            Assert.AreEqual(40.5, order.GetTotalPrice((amount) => amount / 10.0));
+
+            Assert.AreEqual(43.0, order.GetTotalPrice((amount) => amount > 5.0 ? 2.0 : 0.0));
+        }
+    }
+}
