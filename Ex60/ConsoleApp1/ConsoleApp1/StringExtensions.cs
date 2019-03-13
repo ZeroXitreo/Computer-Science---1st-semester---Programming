cbc1de208be9ae9ab561873bf1be330f739a3f25
Devulak <Devulak@users.noreply.github.com>
2019-03-13 08:54:48 +0100

    Complete Ex60A1

---
+++ Ex60/ConsoleApp1/ConsoleApp1/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public static class StringExtensions
+    {
+        public static string Shift(this string str, int shift)
+        {
+            string newString = str.Substring(str.Length - shift, shift);
+            newString += str.Substring(0, str.Length - shift);
+            return newString;
+        }
+    }
+}
---
+++ Ex60/ConsoleApp1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using ConsoleApp1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ShiftStringTest01()
+        {
+            // Arrange
+            string original = "This is my life";
+            // Act
+            string shifted = original.Shift(0);
+            // Assert
+            Assert.AreEqual("This is my life", shifted);
+        }
+        [TestMethod]
+        public void ShiftStringTest02()
+        {
+            // Arrange
+            string original = "This is my life";
+            // Act
+            string shifted = original.Shift(1);
+            // Assert
+            Assert.AreEqual("eThis is my lif", shifted);
+        }
+        [TestMethod]
+        public void ShiftStringTest03()
+        {
+            // Arrange
+            string original = "This is my life";
+            // Act
+            string shifted = original.Shift(5);
+            // Assert
+            Assert.AreEqual(" lifeThis is my", shifted);
+        }
+        [TestMethod]
+        public void ShiftStringTest04()
+        {
+            // Arrange
+            string original = "This is my life";
+            // Act
+            string shifted = original.Shift(14);
+            // Assert
+            Assert.AreEqual("his is my lifeT", shifted);
+        }
+        [TestMethod]
+        public void ShiftStringTest05()
+        {
+            // Arrange
+            string original = "This is my life";
+            // Act
+            string shifted = original.Shift(15);
+            // Assert
+            Assert.AreEqual("This is my life", shifted);
+        }
+    }
+}
